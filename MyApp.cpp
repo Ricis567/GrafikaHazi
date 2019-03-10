2cae499ed12566d7d42c6a75c46f268dfc074095
Török Richárd Dávid <hjlkec@inf.elte.hu>
2019-03-10 10:51:05 +0100

    source codes

---
+++ MyApp.cpp
@@ -0,0 +1,336 @@
+#include "MyApp.h"
+#include "GLUtils.hpp"
+
+#include <math.h>
+
+CMyApp::CMyApp(void)
+{
+	m_vaoID = 0;
+	m_vboID = 0;
+	m_programID = 0;
+}
+
+
+CMyApp::~CMyApp(void)
+{
+}
+
+bool CMyApp::Init()
+{
+	// törlési szín legyen kékes
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	glEnable(GL_CULL_FACE); // kapcsoljuk be a hatrafele nezo lapok eldobasat
+	glEnable(GL_DEPTH_TEST); // mélységi teszt bekapcsolása (takarás)
+	glCullFace(GL_BACK); // GL_BACK: a kamerától "elfelé" nézõ lapok, GL_FRONT: a kamera felé nézõ lapok
+
+	//
+	// geometria letrehozasa
+	//
+
+	Vertex vert[] =
+	{ 
+		//1. piramis
+
+		//alja
+		{glm::vec3(-1, -1, 1), glm::vec3(1, 0, 0)},
+		{glm::vec3(-1, -1,-1), glm::vec3(0, 1, 0)},
+		{glm::vec3(1 , -1, 1), glm::vec3(0, 1, 0)},
+
+		{glm::vec3(-1,-1, -1), glm::vec3(0, 1, 0)},
+		{glm::vec3( 1, -1,-1), glm::vec3(1, 0, 0)},
+		{glm::vec3( 1, -1, 1), glm::vec3(0, 1, 0)},
+
+		// 1. háromszög
+		//          x,  y, z             R, G, B
+		{glm::vec3(-1, -1, 1), glm::vec3(1, 0, 0)},
+		{glm::vec3(1 , -1, 1), glm::vec3(0, 1, 0)},
+		{glm::vec3(0 ,  1, 0), glm::vec3(0, 0, 1)}, // csúcs
+
+		// 2. háromszög
+		{glm::vec3( 1, -1, 1), glm::vec3(0, 1, 0)},
+		{glm::vec3( 1, -1,-1), glm::vec3(1, 0, 0)},
+		{glm::vec3( 0,  1, 0), glm::vec3(0, 0, 1)},
+
+		// 3. háromszög
+		{glm::vec3( 1, -1,-1), glm::vec3(1, 0, 0)},
+		{glm::vec3(-1, -1,-1), glm::vec3(0, 1, 0)},
+		{glm::vec3( 0,  1, 0), glm::vec3(0, 0, 1)},
+
+		// 4. háromszög
+		{glm::vec3(-1, -1,-1), glm::vec3(0, 1, 0)},
+		{glm::vec3(-1, -1, 1), glm::vec3(1, 0, 0)},
+		{glm::vec3(	0,  1, 0), glm::vec3(0, 0, 1)},
+	};
+
+	// 1 db VAO foglalasa
+	glGenVertexArrays(1, &m_vaoID);
+	
+	
+	// a frissen generált VAO beallitasa aktívnak
+	glBindVertexArray(m_vaoID);
+	
+	// hozzunk létre egy új VBO erõforrás nevet
+	glGenBuffers(1, &m_vboID); 
+	glBindBuffer(GL_ARRAY_BUFFER, m_vboID); // tegyük "aktívvá" a létrehozott VBO-t
+
+
+	// töltsük fel adatokkal az aktív VBO-t
+	glBufferData( GL_ARRAY_BUFFER,	// az aktív VBO-ba töltsünk adatokat
+				  sizeof(vert),		// ennyi bájt nagyságban
+				  vert,	// errõl a rendszermemóriabeli címrõl olvasva
+				  GL_STATIC_DRAW);	// úgy, hogy a VBO-nkba nem tervezünk ezután írni és minden kirajzoláskor felhasnzáljuk a benne lévõ adatokat
+	
+
+	// VAO-ban jegyezzük fel, hogy a VBO-ban az elsõ 3 float sizeof(Vertex)-enként lesz az elsõ attribútum (pozíció)
+	glEnableVertexAttribArray(0); // ez lesz majd a pozíció
+	glVertexAttribPointer(
+		0,				// a VB-ben található adatok közül a 0. "indexû" attribútumait állítjuk be
+		3,				// komponens szam
+		GL_FLOAT,		// adatok tipusa
+		GL_FALSE,		// normalizalt legyen-e
+		sizeof(Vertex),	// stride (0=egymas utan)
+		0				// a 0. indexû attribútum hol kezdõdik a sizeof(Vertex)-nyi területen belül
+	); 
+
+	// a második attribútumhoz pedig a VBO-ban sizeof(Vertex) ugrás után sizeof(glm::vec3)-nyit menve újabb 3 float adatot találunk (szín)
+	glEnableVertexAttribArray(1); // ez lesz majd a szín
+	glVertexAttribPointer(
+		1,
+		3, 
+		GL_FLOAT,
+		GL_FALSE,
+		sizeof(Vertex),
+		(void*)(sizeof(glm::vec3)) );
+
+	glBindVertexArray(0); // feltöltüttük a VAO-t, kapcsoljuk le
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // feltöltöttük a VBO-t is, ezt is vegyük le
+
+	//
+	// shaderek betöltése
+	//
+	GLuint vs_ID = loadShader(GL_VERTEX_SHADER,		"myVert.vert");
+	GLuint fs_ID = loadShader(GL_FRAGMENT_SHADER,	"myFrag.frag");
+
+	// a shadereket tároló program létrehozása
+	m_programID = glCreateProgram();
+
+	// adjuk hozzá a programhoz a shadereket
+	glAttachShader(m_programID, vs_ID);
+	glAttachShader(m_programID, fs_ID);
+
+	// VAO-beli attribútumok hozzárendelése a shader változókhoz
+	// FONTOS: linkelés elõtt kell ezt megtenni!
+	glBindAttribLocation(	m_programID,	// shader azonosítója, amibõl egy változóhoz szeretnénk hozzárendelést csinálni
+							0,				// a VAO-beli azonosító index
+							"vs_in_pos");	// a shader-beli változónév
+	glBindAttribLocation( m_programID, 1, "vs_in_col");
+
+	// illesszük össze a shadereket (kimenõ-bemenõ változók összerendelése stb.)
+	glLinkProgram(m_programID);
+
+	// linkeles ellenorzese
+	GLint infoLogLength = 0, result = 0;
+
+	glGetProgramiv(m_programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if ( GL_FALSE == result )
+	{
+		std::vector<char> ProgramErrorMessage( infoLogLength );
+		glGetProgramInfoLog(m_programID, infoLogLength, NULL, &ProgramErrorMessage[0]);
+		fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
+		
+		char* aSzoveg = new char[ProgramErrorMessage.size()];
+		memcpy( aSzoveg, &ProgramErrorMessage[0], ProgramErrorMessage.size());
+
+		std::cout << "[app.Init()] Sáder Huba panasza: " << aSzoveg << std::endl;
+
+		delete aSzoveg;
+	}
+
+	// mar nincs ezekre szukseg
+	glDeleteShader( vs_ID );
+	glDeleteShader( fs_ID );
+
+	//
+	// egyéb inicializálás
+	//
+
+	// vetítési mátrix létrehozása
+	m_matProj = glm::perspective( 45.0f, 640/480.0f, 1.0f, 1000.0f );
+
+	// shader-beli transzformációs mátrixok címének lekérdezése
+	m_loc_world = glGetUniformLocation( m_programID, "world");
+	m_loc_view  = glGetUniformLocation( m_programID, "view" );
+	m_loc_proj  = glGetUniformLocation( m_programID, "proj" );
+	
+	m_loc_time = glGetUniformLocation(m_programID, "time");
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+	glDeleteBuffers(1, &m_vboID);
+	glDeleteVertexArrays(1, &m_vaoID);
+
+	glDeleteProgram( m_programID );
+}
+
+void CMyApp::Update()
+{
+	// nézeti transzformáció beállítása
+	m_matView = glm::lookAt(glm::vec3( look_at_from_x,  look_at_from_y,  look_at_from_z),		// honnan nézzük a színteret
+							glm::vec3( 0,  0,  0),		// a színtér melyik pontját nézzük
+							glm::vec3( 0,  1,  0));		// felfelé mutató irány a világban
+}
+
+
+void CMyApp::Render()
+{
+	// töröljük a frampuffert (GL_COLOR_BUFFER_BIT) és a mélységi Z puffert (GL_DEPTH_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// shader bekapcsolasa
+	glUseProgram( m_programID );
+
+	// shader parameterek beállítása
+	/*
+
+	GLM transzformációs mátrixokra példák:
+		glm::rotate<float>( szög, glm::vec3(tengely_x, tengely_y, tengely_z) ) <- tengely_{xyz} körüli elforgatás
+		glm::translate<float>( glm::vec3(eltol_x, eltol_y, eltol_z) ) <- eltolás
+		glm::scale<float>( glm::vec3(s_x, s_y, s_z) ) <- léptékezés
+
+	*/
+	// m_matWorld = glm::mat4(1.0f);
+	float scaler = sinf(SDL_GetTicks() / 1000.f) * 0.5f + 1.f;
+
+	m_matWorld = glm::translate<float>(glm::vec3(x_offset1, 0, 0)) *
+				 glm::scale<float>(glm::vec3(scaler, 1, scaler)) *
+				 glm::rotate<float>(static_cast<float>(SDL_GetTicks()) / 10.f * (M_PI / 180.f), glm::vec3(0, 1, 0));
+				 
+	m_time = sinf(SDL_GetTicks() / 1000.f) * 0.5f + 0.5f;
+		
+
+	// majd küldjük át a megfelelõ mátrixokat!
+	glUniformMatrix4fv( m_loc_world,// erre a helyre töltsünk át adatot
+						1,			// egy darab mátrixot
+						GL_FALSE,	// NEM transzponálva
+						&(m_matWorld[0][0]) ); // innen olvasva a 16 x sizeof(float)-nyi adatot
+	glUniformMatrix4fv( m_loc_view,  1, GL_FALSE, &( m_matView[0][0]) );
+	glUniformMatrix4fv( m_loc_proj,  1, GL_FALSE, &( m_matProj[0][0]) );
+
+	glUniform1fv(m_loc_time, 1, &m_time);
+
+	// kapcsoljuk be a VAO-t (a VBO jön vele együtt)
+	glBindVertexArray(m_vaoID);
+
+	// kirajzolás
+	glDrawArrays(	GL_TRIANGLES,	// rajzoljunk ki háromszöglista primitívet
+					0,				// a VB elsõ eleme legyen az elsõ kiolvasott vertex
+					18);				// és 6db csúcspont segítségével rajzoljunk háromszöglistát
+
+	/*
+	// VAO kikapcsolasa
+	glBindVertexArray(0);
+
+	// shader kikapcsolasa
+	glUseProgram( 0 );
+	*/
+	/////////////////////////////////////////////////////////////////////
+
+	m_matWorld = glm::translate<float>(glm::vec3(x_offset2, 0, 0)) *
+				 glm::scale<float>(glm::vec3(scaler, 1, scaler)) *
+				 glm::rotate<float>((-1.f) * static_cast<float>(SDL_GetTicks()) / 10.f * (M_PI / 180.f), glm::vec3(0, 1, 0));
+
+	glUniformMatrix4fv(m_loc_world, 1, GL_FALSE, &(m_matWorld[0][0]));
+	glUniformMatrix4fv(m_loc_view,  1, GL_FALSE,  &(m_matView[0][0]));
+	glUniformMatrix4fv(m_loc_proj,  1, GL_FALSE,  &(m_matProj[0][0]));
+
+
+	glDrawArrays(GL_TRIANGLES, 0, 18);
+
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+
+void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
+{
+	double r_horiz = sqrt(pow(look_at_from_z, 2) + pow(look_at_from_x, 2));
+	double r_vert  = sqrt(pow(look_at_from_z, 2) + pow(look_at_from_y, 2));
+
+	switch (key.keysym.sym)
+	{
+	case SDLK_w : 
+
+		look_at_from_x *= 0.9f;
+		look_at_from_y *= 0.9f;
+		look_at_from_z *= 0.9f;
+		break;
+	case SDLK_a:
+		
+		deg_horiz += 5;
+		look_at_from_x = r_horiz * cos(deg_horiz * (M_PI / 180));
+		look_at_from_z = r_horiz * sin(deg_horiz * (M_PI / 180));
+		break;
+	case SDLK_s:
+
+		look_at_from_x *= 1.1f;
+		look_at_from_y *= 1.1f;
+		look_at_from_z *= 1.1f;
+		break;
+	case SDLK_d:
+
+		deg_horiz -= 5;
+		look_at_from_x = r_horiz * cos(deg_horiz * (M_PI / 180));
+		look_at_from_z = r_horiz * sin(deg_horiz * (M_PI / 180));
+		break;
+	case SDLK_UP:
+
+		deg_vert += 5;
+		look_at_from_z = r_vert * cos(deg_vert * (M_PI / 180));
+		look_at_from_y = r_vert * sin(deg_vert * (M_PI / 180));
+		// ++look_at_from_y;
+		break;
+	case SDLK_DOWN:
+
+		deg_vert -= 5;
+		look_at_from_z = r_vert * cos(deg_vert * (M_PI / 180));
+		look_at_from_y = r_vert * sin(deg_vert * (M_PI / 180));
+		// --look_at_from_y;
+		break;
+	}
+}
+
+void CMyApp::KeyboardUp(SDL_KeyboardEvent& key)
+{
+}
+
+void CMyApp::MouseMove(SDL_MouseMotionEvent& mouse)
+{
+
+}
+
+void CMyApp::MouseDown(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseUp(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseWheel(SDL_MouseWheelEvent& wheel)
+{
+}
+
+// a két paraméterbe az új ablakméret szélessége (_w) és magassága (_h) található
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h);
+
+	m_matProj = glm::perspective(  45.0f,		// 90 fokos nyilasszog
+									_w/(float)_h,	// ablakmereteknek megfelelo nezeti arany
+									0.01f,			// kozeli vagosik
+									100.0f);		// tavoli vagosik
+}
---
+++ MyApp.h
@@ -0,0 +1,74 @@
+#pragma once
+
+// GLEW
+#include <GL/glew.h>
+
+// SDL
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform2.hpp>
+
+class CMyApp
+{
+public:
+	CMyApp(void);
+	~CMyApp(void);
+
+	bool Init();
+	void Clean();
+
+	void Update();
+	void Render();
+
+	void KeyboardDown(SDL_KeyboardEvent&);
+	void KeyboardUp(SDL_KeyboardEvent&);
+	void MouseMove(SDL_MouseMotionEvent&);
+	void MouseDown(SDL_MouseButtonEvent&);
+	void MouseUp(SDL_MouseButtonEvent&);
+	void MouseWheel(SDL_MouseWheelEvent&);
+	void Resize(int, int);
+protected:
+	// shaderekhez szükséges változók
+	GLuint m_programID; // shaderek programja
+
+	// OpenGL-es dolgok
+	GLuint m_vaoID; // vertex array object erõforrás azonosító
+	GLuint m_vboID; // vertex buffer object erõforrás azonosító
+
+	// transzformációs mátrixok
+	glm::mat4 m_matWorld;
+	glm::mat4 m_matView;
+	glm::mat4 m_matProj;
+
+	GLfloat m_time = 0;
+
+
+	// mátrixok helye a shaderekben
+	GLuint	m_loc_world;
+	GLuint	m_loc_view;
+	GLuint	m_loc_proj;
+
+	GLuint m_loc_time;
+
+private:
+	double look_at_from_x = 0;
+	double look_at_from_y = 0;
+	double look_at_from_z = 5;
+
+	double deg_horiz = 90;
+	double deg_vert = 0;
+
+	int x_offset1 = -2;
+	int x_offset2 = 2;
+
+	struct Vertex
+	{
+		glm::vec3 p;
+		glm::vec3 c;
+	};
+};
+
